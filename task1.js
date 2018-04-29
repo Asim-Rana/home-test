e5b59b1c02086dce6e4a4deb7d5fe9e20bafc031
Asim-Rana <asim.irshad16@gmail.com>
2018-04-30 01:58:10 +0500

    task 1 changes

--- task1.js
+++ task1.js
@@ -1,7 +1,6 @@
-var http = require('http');
-var url = require('url');
 var cheerio = require('cheerio');
 var express = require('express');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -18,7 +17,42 @@ router.get('/I/want/title', function(req, res) {
             addresses = [addresses];
         }
         var htmlResponse = '<html><head></head><body><h1> Following are the titles of given websites: </h1><ul>';
-        res.json({ message: queryStringParams.address });
+
+        var addTitleInResponse = function(addresses, index, response) {
+
+            if (response) {
+                $ = cheerio.load(response);
+                htmlResponse = htmlResponse + '<li> ' + addresses[index] + ' - "' + $('title').text() + '" </li>';
+            }
+            else {
+                htmlResponse = htmlResponse + '<li> ' + addresses[index] + ' - NO RESPONSE </li>';
+            }
+
+            if ( index < addresses.length - 1 ) {
+                makeHttpRequests(addresses, addTitleInResponse, index+1);
+            }
+            else {
+                htmlResponse = htmlResponse + '</ul></body></html>';
+                res.end(htmlResponse);
+            }
+        };
+
+        function makeHttpRequests(addresses, addTitleInResponse, index) {
+            var address = addresses[index];
+            if ( !address.startsWith('https://') && !address.startsWith('http://') ) {
+                address = 'http://' + address;
+            }
+
+            request(address, function (error, response, body) {
+                if(!error){
+                    addTitleInResponse(addresses, index, body);
+                } else {
+                    addTitleInResponse(addresses, index);
+                }
+            });
+        }
+
+        makeHttpRequests(addresses, addTitleInResponse, 0);
     } else {
         res.json('no address passed in query string "address"');
     }
